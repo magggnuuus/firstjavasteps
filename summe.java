b1b3aa6100848bd2b99d93babed765442110c872
magggnuuus <19mag.nus87@gmail.com>
2021-01-19 17:46:14 +0100

    Added

---
+++ summe.java
@@ -0,0 +1,8 @@
+public class summe {
+    public static void main(String[] args) {
+        int summand1= 5;
+        int summand2= 6;
+        int summe= summand1 + summand2;
+        System.out.println(summe);
+    }
+}
